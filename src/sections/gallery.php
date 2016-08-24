f39c750a75fcc4e6df04ac35dd8969a78e918e54
Emerson Thompson <thompson.emerson@gmail.com>
2016-08-23 22:46:21 -0300

    produtos lua de mel

--- src/sections/gallery.php
+++ src/sections/gallery.php
@@ -2,41 +2,41 @@
   <div id="gallery" class="gallery-content">
 
     <div class="col">
-      <a href="img/gallery/_large-1-1.png" class="item-gallery ripple large">
-        <img src="img/gallery/_large-1-1.png" alt="" />
+      <a href="img/gallery/_large-1-1.jpg" class="item-gallery ripple large">
+        <img src="img/gallery/_large-1-1_thumb.png" alt="" />
       </a>
-      <a href="img/gallery/_small-1-1.png" class="item-gallery ripple small">
-        <img src="img/gallery/_small-1-1.png" alt="" />
+      <a href="img/gallery/_small-1-1.jpg" class="item-gallery ripple small">
+        <img src="img/gallery/_small-1-1_thumb.png" alt="" />
       </a>
-      <a href="img/gallery/_small-1-2.png" class="item-gallery ripple small">
-        <img src="img/gallery/_small-1-2.png" alt="" />
-      </a>
-    </div>
-
-    <div class="col">
-      <a href="img/gallery/_small-2-1.png" class="item-gallery ripple small">
-        <img src="img/gallery/_small-2-1.png" alt="" />
-      </a>
-      <a href="img/gallery/_small-2-2.png" class="item-gallery ripple small">
-        <img src="img/gallery/_small-2-2.png" alt="" />
-      </a>
-      <a href="img/gallery/_small-2-3.png" class="item-gallery ripple small">
-        <img src="img/gallery/_small-2-3.png" alt="" />
-      </a>
-      <a href="img/gallery/_small-2-4.png" class="item-gallery ripple small">
-        <img src="img/gallery/_small-2-4.png" alt="" />
-      </a>
-      <a href="img/gallery/_medium-2-1.png" class="item-gallery ripple medium">
-        <img src="img/gallery/_medium-2-1.png" alt="" />
+      <a href="img/gallery/_small-1-2.jpg" class="item-gallery ripple small">
+        <img src="img/gallery/_small-1-2_thumb.png" alt="" />
       </a>
     </div>
 
     <div class="col">
-      <a href="img/gallery/_medium-3-1.png" class="item-gallery ripple medium">
-        <img src="img/gallery/_medium-3-1.png" alt="" />
+      <a href="img/gallery/_small-2-1.jpg" class="item-gallery ripple small">
+        <img src="img/gallery/_small-2-1_thumb.png" alt="" />
       </a>
-      <a href="img/gallery/_large-3-1.png" class="item-gallery ripple large">
-        <img src="img/gallery/_large-3-1.png" alt="" />
+      <a href="img/gallery/_small-2-2.jpg" class="item-gallery ripple small">
+        <img src="img/gallery/_small-2-2_thumb.png" alt="" />
+      </a>
+      <a href="img/gallery/_small-2-3.jpg" class="item-gallery ripple small">
+        <img src="img/gallery/_small-2-3_thumb.png" alt="" />
+      </a>
+      <a href="img/gallery/_small-2-4.jpg" class="item-gallery ripple small">
+        <img src="img/gallery/_small-2-4_thumb.png" alt="" />
+      </a>
+      <a href="img/gallery/_medium-2-1.jpg" class="item-gallery ripple medium">
+        <img src="img/gallery/_medium-2-1_thumb.png" alt="" />
+      </a>
+    </div>
+
+    <div class="col">
+      <a href="img/gallery/_medium-3-1.jpg" class="item-gallery ripple medium">
+        <img src="img/gallery/_medium-3-1_thumb.png" alt="" />
+      </a>
+      <a href="img/gallery/_large-3-1.jpg" class="item-gallery ripple large">
+        <img src="img/gallery/_large-3-1_thumb.png" alt="" />
       </a>
     </div>
 
--- src/sections/gift.php
+++ src/sections/gift.php
@@ -29,50 +29,175 @@
         <h3 class="title"><span>Lua de Mel</span></h3>
 
         <div class="products products--js">
+
           <div class="product">
-            <a href="#">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-a50cfa3f-ec9d-42a2-82fc-236fc3cc360a" name="MP-payButton">
               <figure class="thumbnail">
-                <img src="img/shop/t-shirt.png" alt="" />
+                <img src="img/shop/lua-de-mel/Garrafa_de_vinho.jpg" alt="" />
               </figure>
               <div class="info">
-                <div class="name">T-Shirt</div>
-                <div class="price">5,00€</div>
+                <div class="name">Garrafa de vinho</div>
+                <div class="price">R$ 75,00</div>
               </div>
             </a>
           </div>
+
           <div class="product">
-            <a href="#">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-3f965178-8049-45e3-9c26-bb790f2c9744" name="MP-payButton">
               <figure class="thumbnail">
-                <img src="img/shop/pants.png" alt="" />
+                <img src="img/shop/lua-de-mel/Passeio_de_Barco.jpg" alt="" />
               </figure>
               <div class="info">
-                <div class="name">Pants Forclaz</div>
-                <div class="price">30,00€</div>
+                <div class="name">Passeio de Barco</div>
+                <div class="price">R$ 150,00</div>
               </div>
             </a>
           </div>
+
           <div class="product">
-            <a href="#">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-2b7a5058-8b6a-4724-8ce1-2ef583cb32c3" name="MP-payButton">
               <figure class="thumbnail">
-                <img src="img/shop/backpack.png" alt="" />
+                <img src="img/shop/lua-de-mel/Jantar_Romantico.jpg" alt="" />
               </figure>
               <div class="info">
-                <div class="name">Backpack</div>
-                <div class="price">60,00€</div>
+                <div class="name">Jantar Romântico</div>
+                <div class="price">R$ 200,00</div>
               </div>
             </a>
           </div>
+
           <div class="product">
-            <a href="#">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-589bcd01-1762-4ec1-99a7-f9f884311253" name="MP-payButton">
               <figure class="thumbnail">
-                <img src="img/shop/backpack.png" alt="" />
+                <img src="img/shop/lua-de-mel/Degustacao_de_vinhos.jpg" alt="" />
               </figure>
               <div class="info">
-                <div class="name">Backpack</div>
-                <div class="price">60,00€</div>
+                <div class="name">Degustação de Vinhos</div>
+                <div class="price">R$ 150,00</div>
               </div>
             </a>
           </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-40509120-c627-41ff-8f0d-08265276d0ad" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/Compras_em_mercadinho.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">Compras em Mercadinho</div>
+                <div class="price">R$ 365,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-91a3b437-0707-4d52-aebe-e6680b8ef7d5" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/Visita_a_museu_europeu2.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">Visita a Museus Europeus</div>
+                <div class="price">R$ 150,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-044d24be-08a4-4a28-aab2-b6841ca7df25" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/cafe_da_manha.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">Café da Manhã</div>
+                <div class="price">R$ 40,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-bcf97517-962e-45fc-89e2-828c443a16b6" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/Almoco.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">Almoço Romântico</div>
+                <div class="price">R$ 100,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-31b5f439-2cb6-4927-b25b-2bc1a861628e" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/City_Tuor.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">City Tuor</div>
+                <div class="price">R$ 200,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-06615885-1a58-4cdd-bb8d-84da6e80f905" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/Translado.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">Translado Hotel/Aeroporto</div>
+                <div class="price">R$ 80,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-1e1d12ec-6690-4649-ac13-dc0a25d886e9" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/Oceanario.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">Ingressos para o Oceanário</div>
+                <div class="price">R$ 120,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-73a8235f-8d3c-401d-98fb-518050ab74ff" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/Trem.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">Passagem de Trem</div>
+                <div class="price">R$ 100,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-48bf4ca2-60db-43b2-9dc1-31d075925979" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/Europa.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">3 Diárias de Hotel na Europa</div>
+                <div class="price">R$ 300,00</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="product">
+            <a mp-mode="dftl" href="https://www.mercadopago.com/mlb/checkout/start?pref_id=162442919-6be6d94e-cb8f-4d32-b468-ecfd08a19761" name="MP-payButton">
+              <figure class="thumbnail">
+                <img src="img/shop/lua-de-mel/passeios_turisticos.jpg" alt="" />
+              </figure>
+              <div class="info">
+                <div class="name">1 Dia de Passeios Turisticos</div>
+                <div class="price">R$ 250,00</div>
+              </div>
+            </a>
+          </div>
+
         </div>
       </div>
 
