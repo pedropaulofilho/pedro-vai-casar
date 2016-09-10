828393448ec5c719769f89beb885d5525b3d5118
Emerson Thompson <thompson.emerson@gmail.com>
2016-09-10 20:12:15 -0300

    goto gift

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,16 @@ $(document).ready(function(){
   console.log('Arroxa Pedrão!');
 
   // gallery
-  $("#gallery").lightGallery({
+  $('#gallery').lightGallery({
     selector: '.item-gallery',
     cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
+  });
+
+  $('.goto').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("#gift").offset().top
+    }, 1000);
   });
 
 });
