6c0d0f98b13beeff6035c63a7a6f5fe57410c68e
Emerson Thompson <thompson.emerson@gmail.com>
2016-09-10 17:25:35 -0300

    Fix build de imagens
    O build de imagens vinha dando problemas, não passava todas as imagens para
    o dist.
    Com isso foi colocado apenas um `copy` sem minificar a imagem.

--- gulp_tasks/images.js
+++ gulp_tasks/images.js
@@ -1,19 +1,17 @@
 var gulp            = require( 'gulp' ),
     plumber         = require( 'gulp-plumber' ),
-    cache           = require( 'gulp-cache' ),
-    imagemin        = require( 'gulp-imagemin' ),
     browserSync     = require( 'browser-sync' );
 
 
 
 // Images
 gulp.task( 'images', function() {
-  gulp.src( [ gulp.paths.images ] )
-    .pipe( cache( imagemin({
-      optimizationLevel: 3,
-      progressive: true,
-      interlaced: true
-    })))
+  return gulp.src( [ gulp.paths.images ] )
+    .pipe( plumber({
+      errorHandler: function( error ) {
+        console.log( error.message );
+        this.emit( 'end' );
+    }}))
     .pipe( gulp.dest( gulp.paths.imagesDest ) )
     .pipe( browserSync.reload( {stream:true} ) );
 });
